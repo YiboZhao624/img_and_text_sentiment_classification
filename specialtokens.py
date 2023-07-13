b80afac2034a1fb321bd5bcb75fc6982f7c8ebfb
赵艺博 <90241849+zhaoyib@users.noreply.github.com>
2023-07-13 16:01:02 +0800

    special tokens for the tokenizer

---
+++ specialtokens.py
@@ -0,0 +1,18 @@
+
+class SpecialTokens:
+    """special tokens
+    """
+    PAD_TOKEN = "[PAD]" # padding token
+    UNK_TOKEN = "[UNK]" # unknown token
+    SOS_TOKEN = "[SOS]" # start token
+    EOS_TOKEN = "[EOS]" # end token
+    NON_TOKEN = "[NON]" # non-terminal token
+    BRG_TOKEN = "[BRG]" # equation connecting token
+    OPT_TOKEN = "[OPT]" # operator mask token
+    CLS_TOKEN = "[CLS]"
+    SEP_TOKEN = "[SEP]"
+    MASK_TOKEN = "[MASK]"
+    NUM_TOKEN = '[NUM]'
+    USER_TOKEN = '[USER]'
+    TAG_TOKEN = '[TAG]'
+    
